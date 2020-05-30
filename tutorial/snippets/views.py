25e74efc05b80536155ee2be307c0310afb896fe
Paahn <pjhatzis@gmail.com>
2020-05-29 18:27:53 -0700

    using the set of already mixed-in generic views from REST to reduce the amount of code written for the same views result

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,38 +1,18 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 # Create your views here
 
-# We are now using the REST framework's mixin class based views, which 
-# give us the ability to write reusable bits of behaviour.
-# The core functionality now lives in GenericAPIView
+# The REST framework provides us a set of already mixed-in generic views that we can use to trim down
+# our views code even more
 
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+# basically we don't even need to define methods for HTTP actions anymore, just pass the generic view into the class
+
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
