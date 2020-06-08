9b08217bcbba172c85591c6cc7e13261c75f2032
Paahn <pjhatzis@gmail.com>
2020-06-08 14:46:31 -0700

    hyperlinking our api

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -3,7 +3,7 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework import generics
 from django.contrib.auth.models import User
-from rest_framework import permissions
+from rest_framework import permissions, renderers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -45,3 +45,11 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
